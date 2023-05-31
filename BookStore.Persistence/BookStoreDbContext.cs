238bd71234b3447e41b6e032a62d7780ece6100c
99Astral99 <strik4462@gmail.com>
2023-05-31 21:44:59 +0300

    Added auth with jwt bearer token

--- BookStore.Persistence/BookStoreDbContext.cs
+++ BookStore.Persistence/BookStoreDbContext.cs
@@ -1,13 +1,15 @@
 ﻿using BookStore.Application.Interfaces;
 using BookStore.Domain;
+using BookStore.Domain.Identity;
 using BookStore.Persistence.EntityTypeConfiguration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookStore.Persistence
 {
-    public class BookStoreDbContext : DbContext, IBookStoreDbContext
+    public class BookStoreDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>, IBookStoreDbContext
     {
-        public DbSet<Book> Books { get; set; }
         public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options)
             : base(options) { }
 
@@ -64,7 +66,83 @@ namespace BookStore.Persistence
                 });
 
 
+            var user1 = new User()
+            {
+                Id = new Guid("A4A67768-DEDA-43BC-A545-A67D1ABDA650"),
+                Email = "customer@gmail.com",
+                EmailConfirmed = true,
+                FirstName = "Dante",
+                LastName = "Sparda",
+                MiddleName = "Spardovich",
+                UserName = "Customer111",
+                NormalizedUserName = "CUSTOMER111",
+            };
+
+            var user2 = new User()
+            {
+                Id = new Guid("12539B7B-B1E6-4A04-BE98-6921487CAEF3"),
+                Email = "admin@gmail.com",
+                EmailConfirmed = true,
+                FirstName = "Vergil",
+                LastName = "Sparda",
+                MiddleName = "Spardovich",
+                UserName = "Admin111",
+                NormalizedUserName = "ADMIN111",
+            };
+
+            PasswordHasher<User> ph = new PasswordHasher<User>();
+            user1.PasswordHash = ph.HashPassword(user1, "Customer123");
+            user2.PasswordHash = ph.HashPassword(user2, "Admin123");
+
+            builder.Entity<User>().HasData(user1);
+            builder.Entity<User>().HasData(user2);
+
+            builder.Entity<IdentityRole<Guid>>().HasData(
+                new IdentityRole<Guid>
+                {
+                    Id = new Guid("76BA16A2-158A-46EF-89E7-24E8684AAB20"),
+                    Name = "Customer",
+                    NormalizedName = "CUSTOMER",
+                    ConcurrencyStamp = "76BA16A2-158A-46EF-89E7-24E8684AAB20"
+                });
+
+            builder.Entity<IdentityRole<Guid>>().HasData(
+                new IdentityRole<Guid>
+                {
+                    Id = new Guid("417EA524-3412-4929-8533-74354E887CC5"),
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
+                    ConcurrencyStamp = "417EA524-3412-4929-8533-74354E887CC5"
+                });
+
+
+            builder.Entity<IdentityRole<Guid>>().HasData(
+                new IdentityRole<Guid>
+                {
+                    Id = new Guid("8C11D40A-1D5A-4E4F-9A15-B20A0B045C44"),
+                    Name = "Moderator",
+                    NormalizedName = "MODERATOR",
+                    ConcurrencyStamp = "8C11D40A-1D5A-4E4F-9A15-B20A0B045C44"
+                });
+
+            builder.Entity<IdentityUserRole<Guid>>().HasData(
+                new IdentityUserRole<Guid>
+                {
+                    RoleId = new Guid("76BA16A2-158A-46EF-89E7-24E8684AAB20"),
+                    UserId = new Guid("A4A67768-DEDA-43BC-A545-A67D1ABDA650")
+                });
+
+            builder.Entity<IdentityUserRole<Guid>>().HasData(
+                new IdentityUserRole<Guid>
+                {
+                    RoleId = new Guid("417EA524-3412-4929-8533-74354E887CC5"),
+                    UserId = new Guid("12539B7B-B1E6-4A04-BE98-6921487CAEF3")
+                });
+
+
             base.OnModelCreating(builder);
         }
+
+        public DbSet<Book> Books { get; set; }
     }
 }
--- BookStoreWebApi/Controllers/BookStoreController.cs
+++ BookStoreWebApi/Controllers/BookStoreController.cs
@@ -5,71 +5,68 @@ using BookStore.Application.BookStore.Commands.UpdateBook;
 using BookStore.Application.BookStore.Queries.GetBookInfo;
 using BookStore.Application.BookStore.Queries.GetBookList;
 using BookStoreWebApi.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BookStoreWebApi.Controllers
 {
-    [Route("api/[controller]")]
-    public class BookStoreController:BaseController
-    {
-        private readonly IMapper _mapper;
-        public BookStoreController(IMapper mapper) =>
-            _mapper = mapper;
+	[Authorize]
+	[Route("api/[controller]")]
+	public class BookStoreController : BaseController
+	{
+		private readonly IMapper _mapper;
+		public BookStoreController(IMapper mapper) =>
+			_mapper = mapper;
+		
+		[HttpGet]
+		public async Task<ActionResult<BookListVm>> GetAll()
+		{
+			var query = new GetBookListQuery();
+			var vm = await Mediator.Send(query);
+			return Ok(vm);
+		}
+	
+		[HttpGet("{id}")]
+		public async Task<ActionResult<BookInfoVm>> Get(int id)
+		{
+			var query = new GetBookInfoQuery()
+			{
+				Id = id
+			};
 
-        [HttpGet]
-        public async Task<ActionResult<BookListVm>> GetAll()
-        {
-            var query = new GetBookListQuery();
-            var vm = await Mediator.Send(query);
-            return Ok(vm);
-        }
+			var vm = await Mediator.Send(query);
+			return Ok(vm);
+		}
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<BookInfoVm>> Get(int id)
-        {
-            var query = new GetBookInfoQuery()
-            {
-                Id = id
-            };
+		[HttpPost]
+		public async Task<ActionResult<int>> Create([FromBody] CreateBookDto createBookDto)
+		{
+			var command = _mapper.Map<CreateBookCommand>(createBookDto);
+			var bookId = await Mediator.Send(command);
 
-            var vm = await Mediator.Send(query);
-            return Ok(vm);
-        }
+			return Ok(bookId);
+		}
 
-        [HttpPost]
-        public async Task<ActionResult<int>> Create([FromBody] CreateBookDto createBookDto)
-        {
-            var command = _mapper.Map<CreateBookCommand>(createBookDto);
-            var bookId = await Mediator.Send(command);
+		[HttpPut]
+		public async Task<IActionResult> Update([FromBody] UpdateBookDto updateBookDto)
+		{
+			var command = _mapper.Map<UpdateBookCommand>(updateBookDto);
+			await Mediator.Send(command);
+			return NoContent();
+		}
 
-            return Ok(bookId);
-        }
+		[HttpDelete("{id:int}")]
+		public async Task<IActionResult> Delete(int id)
+		{
+			var command = new DeleteBookCommand()
+			{
+				Id = id
+			};
 
-        [HttpPut]
-        public async Task<IActionResult> Update([FromBody] UpdateBookDto updateBookDto)
-        {
-            var command = _mapper.Map<UpdateBookCommand>(updateBookDto);
-            await Mediator.Send(command);
-            return NoContent();
-        }
+			await Mediator.Send(command);
 
-        [HttpDelete("{id:int}")]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var command = new DeleteBookCommand()
-            {
-                Id = id
-            };
-
-            await Mediator.Send(command);
-
-            return NoContent();
-        }
-    }
+			return NoContent();
+		}
+	}
 }
 
